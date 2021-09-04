cf3b2852f310f28ff203b97345cf0f0b42cb66a8
lisarozo <erozo190@gmail.com>
2021-09-03 21:36:05 -0500

    filled in questions and answers

--- script.js
+++ script.js
@@ -5,24 +5,24 @@ var questions = [
         correctAnswer: "none"
     },
     {
-        questionText: "How many bones are in a shark?",
-        answerChoices: ["none", "1", "2", "3"],
-        correctAnswer: "none"
+        questionText: "Name the biggest island in the world",
+        answerChoices: ["Greenland", "New Guinea", "Borneo", "Madagascar"],
+        correctAnswer: "Greenland"
     },
     {
-        questionText: "How many bones are in a shark?",
-        answerChoices: ["none", "1", "2", "3"],
-        correctAnswer: "none"
+        questionText: "Which country produces the most coffee in the world?",
+        answerChoices: ["Brazil", "Colombia", "Vietnam", "Indonesia"],
+        correctAnswer: "Brazil"
     },
     {
-        questionText: "How many bones are in a shark?",
-        answerChoices: ["none", "1", "2", "3"],
-        correctAnswer: "none"
+        questionText: "What color is a Giraffe's tongue?",
+        answerChoices: ["Blue/Black", "Red", "Purple", "Pink"],
+        correctAnswer: "Blue/Black"
     },
     {
-        questionText: "How many bones are in a shark?",
-        answerChoices: ["none", "1", "2", "3"],
-        correctAnswer: "none"
+        questionText: "How many bones are there in the human body?",
+        answerChoices: ["103", "212", "206", "96"],
+        correctAnswer: "206"
     },
 
 ]
